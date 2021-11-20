9ca1a5a8befc5628a4b12e1a0963e7f616775b34
ebfaler <74653573+ebfaler@users.noreply.github.com>
2021-11-20 18:12:33 +0100

    started on js search functionality

--- js/app.js
+++ js/app.js
@@ -6,14 +6,16 @@ const gridContainer = document.querySelector(".grid-container");
 const overlay = document.querySelector(".overlay");
 const modalContainer = document.querySelector(".modal-content");
 const modalClose = document.querySelector(".modal-close")
+const searchBox = document.querySelector(".search");
+
 
 
 // fetch data from API
 fetch(urlAPI)
-.then(res => res.json())
-.then(res => res.results)
-.then(displayEmployees)   //Passing control to the displayEmployees function, which will be created next
-.catch(err => console.log(err))
+    .then(res => res.json())
+    .then(res => res.results)
+    .then(displayEmployees)   //Passing control to the displayEmployees function, which will be created next
+    .catch(err => console.log(err))
 
 
 
@@ -25,13 +27,13 @@ function displayEmployees(employeeData) {
 
     // loop through each employee and create HTML markup
     employees.forEach((employee, index) => {
-    let name = employee.name;
-    let email = employee.email;
-    let city = employee.location.city;
-    let picture = employee.picture;
+        let name = employee.name;
+        let email = employee.email;
+        let city = employee.location.city;
+        let picture = employee.picture;
 
-    // template literals make this so much cleaner!
-    employeeHTML += `
+        // template literals make this so much cleaner!
+        employeeHTML += `
     <div class="card" data-index="${index}">
     <img class="avatar" src="${picture.large}" />
     <div class="text-container">
@@ -53,7 +55,7 @@ function displayModal(index) {
     }, picture } = employees[index];
 
     let date = new Date(dob.date);
-    
+
     const modalHTML = `
     <img class="avatar" src="${picture.large}" />
     <div class="text-container">
@@ -62,7 +64,7 @@ function displayModal(index) {
     <p class="address">${city}</p>
     <hr />
     <p>${phone}</p>
-    <p class="address">${street}, ${state} ${postcode}</p>
+    <p class="address">${street.number}, ${street.name}, ${state}, ${postcode}</p>
     <p>Birthday:
     ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
     </div>
@@ -71,7 +73,7 @@ function displayModal(index) {
 
     overlay.classList.remove("hidden");
     modalContainer.innerHTML = modalHTML;
-    }
+}
 
 
 //Event Listeners
@@ -81,20 +83,58 @@ gridContainer.addEventListener('click', e => {
     // make sure the click is not on the gridContainer itself
     if (e.target !== gridContainer) {
 
-    // select the card element based on its proximity to actual element clicked
-    const card = e.target.closest(".card");
-    const index = card.getAttribute('data-index');
-    displayModal(index);
+        // select the card element based on its proximity to actual element clicked
+        const card = e.target.closest(".card");
+        const index = card.getAttribute('data-index');
+        displayModal(index);
     }
-    });
+});
 
 
 //modalClose click event
 
 modalClose.addEventListener('click', () => {
-     overlay.classList.add("hidden");
-     modalContainer.classList.add("hidden");
-        });
+    overlay.classList.add("hidden");
+});
+
+
+
+// Employees can be filtered by name
+
+searchBox.addEventListener('keyUp', (e) => {
+
+    let searchTerm = e.target.value.toLowerCase();
+    let cards = document.querySelector(".card");
+    let employeeData = cards.querySelector(".name");
+    
+    cards.forEach((employeeData, index) => { 
+        
+        if(employeeData.contains)
+    
+
+
+    }
+    // get employees data
+    //employees.forEach((employeeData, index) =>
+   // if employeeData contains string , name whatever:
+   // select card with same index, switch on or off
+
+
+    // cards.forEach(card => {
+    //     if (card.innerHTML.toLowerCase().includes(searchTerm)) {
+    //        card.style.display = "flex";
+    //     }
+    //     else {
+    //         card.style.display = "none";  
+    //     }
+    // }
+  
+    )
+
+}
+)
+
+
 
 
 
@@ -102,4 +142,3 @@ modalClose.addEventListener('click', () => {
 
 
 
-// Employees can be filtered by name
